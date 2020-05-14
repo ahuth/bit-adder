4788f74ce506ce81b8d16ffac26478840a6378d8
Andrew Huth <andrew@huth.me>
2020-05-14 07:05:56 -0700

    Directly target the labels with CSS

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -27,41 +27,41 @@ export default function App() {
       <fieldset>
         <legend>Input A</legend>
         <div className={styles.switchContainer}>
-          <label>A7 <input checked={inputA[7]} onChange={() => setInputA(flipBit(7))} type="checkbox" /></label>
-          <label>A6 <input checked={inputA[6]} onChange={() => setInputA(flipBit(6))} type="checkbox" /></label>
-          <label>A5 <input checked={inputA[5]} onChange={() => setInputA(flipBit(5))} type="checkbox" /></label>
-          <label>A4 <input checked={inputA[4]} onChange={() => setInputA(flipBit(4))} type="checkbox" /></label>
-          <label>A3 <input checked={inputA[3]} onChange={() => setInputA(flipBit(3))} type="checkbox" /></label>
-          <label>A2 <input checked={inputA[2]} onChange={() => setInputA(flipBit(2))} type="checkbox" /></label>
-          <label>A1 <input checked={inputA[1]} onChange={() => setInputA(flipBit(1))} type="checkbox" /></label>
-          <label>A0 <input checked={inputA[0]} onChange={() => setInputA(flipBit(0))} type="checkbox" /></label>
+          <label className={styles.bit}>A7 <input checked={inputA[7]} onChange={() => setInputA(flipBit(7))} type="checkbox" /></label>
+          <label className={styles.bit}>A6 <input checked={inputA[6]} onChange={() => setInputA(flipBit(6))} type="checkbox" /></label>
+          <label className={styles.bit}>A5 <input checked={inputA[5]} onChange={() => setInputA(flipBit(5))} type="checkbox" /></label>
+          <label className={styles.bit}>A4 <input checked={inputA[4]} onChange={() => setInputA(flipBit(4))} type="checkbox" /></label>
+          <label className={styles.bit}>A3 <input checked={inputA[3]} onChange={() => setInputA(flipBit(3))} type="checkbox" /></label>
+          <label className={styles.bit}>A2 <input checked={inputA[2]} onChange={() => setInputA(flipBit(2))} type="checkbox" /></label>
+          <label className={styles.bit}>A1 <input checked={inputA[1]} onChange={() => setInputA(flipBit(1))} type="checkbox" /></label>
+          <label className={styles.bit}>A0 <input checked={inputA[0]} onChange={() => setInputA(flipBit(0))} type="checkbox" /></label>
         </div>
       </fieldset>
       <fieldset>
         <legend>Input B</legend>
         <div className={styles.switchContainer}>
-          <label>B7 <input checked={inputB[7]} onChange={() => setInputB(flipBit(7))} type="checkbox" /></label>
-          <label>B6 <input checked={inputB[6]} onChange={() => setInputB(flipBit(6))} type="checkbox" /></label>
-          <label>B5 <input checked={inputB[5]} onChange={() => setInputB(flipBit(5))} type="checkbox" /></label>
-          <label>B4 <input checked={inputB[4]} onChange={() => setInputB(flipBit(4))} type="checkbox" /></label>
-          <label>B3 <input checked={inputB[3]} onChange={() => setInputB(flipBit(3))} type="checkbox" /></label>
-          <label>B2 <input checked={inputB[2]} onChange={() => setInputB(flipBit(2))} type="checkbox" /></label>
-          <label>B1 <input checked={inputB[1]} onChange={() => setInputB(flipBit(1))} type="checkbox" /></label>
-          <label>B0 <input checked={inputB[0]} onChange={() => setInputB(flipBit(0))} type="checkbox" /></label>
+          <label className={styles.bit}>B7 <input checked={inputB[7]} onChange={() => setInputB(flipBit(7))} type="checkbox" /></label>
+          <label className={styles.bit}>B6 <input checked={inputB[6]} onChange={() => setInputB(flipBit(6))} type="checkbox" /></label>
+          <label className={styles.bit}>B5 <input checked={inputB[5]} onChange={() => setInputB(flipBit(5))} type="checkbox" /></label>
+          <label className={styles.bit}>B4 <input checked={inputB[4]} onChange={() => setInputB(flipBit(4))} type="checkbox" /></label>
+          <label className={styles.bit}>B3 <input checked={inputB[3]} onChange={() => setInputB(flipBit(3))} type="checkbox" /></label>
+          <label className={styles.bit}>B2 <input checked={inputB[2]} onChange={() => setInputB(flipBit(2))} type="checkbox" /></label>
+          <label className={styles.bit}>B1 <input checked={inputB[1]} onChange={() => setInputB(flipBit(1))} type="checkbox" /></label>
+          <label className={styles.bit}>B0 <input checked={inputB[0]} onChange={() => setInputB(flipBit(0))} type="checkbox" /></label>
         </div>
       </fieldset>
       <fieldset>
         <legend>Output</legend>
         <div className={styles.outputContainer}>
-          <label>O8 <input checked={false} disabled type="checkbox" /></label>
-          <label>O7 <input checked={output[7]} disabled type="checkbox" /></label>
-          <label>O6 <input checked={output[6]} disabled type="checkbox" /></label>
-          <label>O5 <input checked={output[5]} disabled type="checkbox" /></label>
-          <label>O4 <input checked={output[4]} disabled type="checkbox" /></label>
-          <label>O3 <input checked={output[3]} disabled type="checkbox" /></label>
-          <label>O2 <input checked={output[2]} disabled type="checkbox" /></label>
-          <label>O1 <input checked={output[1]} disabled type="checkbox" /></label>
-          <label>O0 <input checked={output[0]} disabled type="checkbox" /></label>
+          <label className={styles.bit}>O8 <input checked={false} disabled type="checkbox" /></label>
+          <label className={styles.bit}>O7 <input checked={output[7]} disabled type="checkbox" /></label>
+          <label className={styles.bit}>O6 <input checked={output[6]} disabled type="checkbox" /></label>
+          <label className={styles.bit}>O5 <input checked={output[5]} disabled type="checkbox" /></label>
+          <label className={styles.bit}>O4 <input checked={output[4]} disabled type="checkbox" /></label>
+          <label className={styles.bit}>O3 <input checked={output[3]} disabled type="checkbox" /></label>
+          <label className={styles.bit}>O2 <input checked={output[2]} disabled type="checkbox" /></label>
+          <label className={styles.bit}>O1 <input checked={output[1]} disabled type="checkbox" /></label>
+          <label className={styles.bit}>O0 <input checked={output[0]} disabled type="checkbox" /></label>
         </div>
       </fieldset>
     </form>
