777fec2ce992e9c5824b9680ceea933090e7b82e
Andrew Huth <andrew@huth.me>
2020-05-14 08:49:39 -0700

    Add readme and comments

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # bit-adder
+
+Implementation (virtually) of the binary adding machine from chapter 12 of [Code, by Charles Petzold](http://www.charlespetzold.com/code/).
--- src/add.ts
+++ src/add.ts
@@ -1,5 +1,6 @@
 import { Bit, and, or, xor } from './circuitry';
 
+// Add 2 1-bit numbers together.
 function halfAdder(augend: Bit, addend: Bit) {
   return {
     sum: xor(augend, addend),
@@ -7,6 +8,8 @@ function halfAdder(augend: Bit, addend: Bit) {
   };
 }
 
+// Add 3 1-bit numbers together, which allows us to add 2 1-bit numbers, plus any initial carry
+// (coming from a previous addition step).
 function fullAdder(a: Bit, b: Bit, carryIn: Bit) {
   const first = halfAdder(a, b);
   const second = halfAdder(carryIn, first.sum);
@@ -16,6 +19,9 @@ function fullAdder(a: Bit, b: Bit, carryIn: Bit) {
   };
 }
 
+// Add two binary numbers together. The output will have one more bit than the inputs. For example,
+// an 8-bit number can be between 0 and 255. Adding two of them together gives us a number between
+// 0 and 510. To representt that sum, we need 9 bits.
 export default function add(inputA: Bit[], inputB: Bit[]): Bit[] {
   const output: Bit[] = [];
   let carry = false;
